c7dd373f62128b47b5af0fa9fe045e2d593dc9ae
Eddie <eddielacrosse2@gmail.com>
2022-10-31 16:52:40 -0400

    Got QR Page to display PNG logo. Still need to get 3D Model to work

--- src/app/qr/qr.page.ts
+++ src/app/qr/qr.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import 'mind-ar/dist/mindar-image.prod.js';
 import 'aframe';
 import 'mind-ar/dist/mindar-image-aframe.prod.js';
+import { timeout } from 'rxjs/operators';
 
 @Component({
   selector: 'app-qr',
@@ -10,16 +11,19 @@ import 'mind-ar/dist/mindar-image-aframe.prod.js';
   styleUrls: ['./qr.page.scss'],
 })
 export class QRPage implements OnInit {
+  aSceneLoaded = false;
 
   constructor(
     private router: Router
   ) { }
 
   ngOnInit() {
-    document.querySelector('a-scene').addEventListener('loaded', function(e) { 
-      console.log(e);
-      
-    });
+    this.loadAScene();
+  }
+  loadAScene() {
+    setTimeout(() => {
+      this.aSceneLoaded = true;
+    }, 1000);
   }
   backToHomepage() {
     this.router.navigateByUrl('home');
