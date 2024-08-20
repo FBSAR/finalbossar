2219a767118e391a0fb6edfb8cf12f5f0b85c154
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2024-08-20 19:13:21 -0400

    Added addition sections for home page test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,19 @@
   <!-- Crowdfunding -->
   <!-- Page Wrapper -->
   <div class="mx-auto h-20 bg-white w-11/12 lg:w-11/12 xl:w-10/12">
-    <h1 class="text-black">Home</h1>
+    <h1 class="text-black">Top News / Cosmics</h1>
+
+  </div>
+  <div class="mx-auto my-2 h-20 bg-white w-11/12 lg:w-11/12 xl:w-10/12">
+    <h1 class="text-black">Tester</h1>
+
+  </div>
+  <div class="mx-auto my-2 h-20 bg-white w-11/12 lg:w-11/12 xl:w-10/12">
+    <h1 class="text-black">Crowdfunding</h1>
+
+  </div>
+  <div class="mx-auto my-2 h-20 bg-white w-11/12 lg:w-11/12 xl:w-10/12">
+    <h1 class="text-black">???</h1>
 
   </div>
 </main>
