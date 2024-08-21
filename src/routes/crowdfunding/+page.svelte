c4748987f89c7289fb921bd1882accec4a7323c5
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2024-08-21 19:17:54 -0400

    Added Headers to all pages, including animation

--- src/routes/crowdfunding/+page.svelte
+++ src/routes/crowdfunding/+page.svelte
@@ -7,11 +7,11 @@
     })
 </script>
 <main>
-  <!-- Page Wrapper -->
-  <div class="mx-auto h-20 bg-white w-11/12 lg:w-11/12 xl:w-10/12">
-    <h1 class="text-black">Crowdfunding</h1>
- 
-   </div>
+  <!-- Header -->
+  <div class="mx-auto w-11/12 lg:w-11/12 xl:w-10/12">
+    <h1 class="text-black text-white jersey-font page-header">CROWDFUNDING</h1>
+    <p class="sm:w-1/2 lg:w-1/2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium quis velit quibusdam molestias perspiciatis quidem ad, doloremque iure rem beatae magni harum tempore pariatur incidunt hic, alias ipsum dicta amet.</p>
+  </div>
 </main>
 <style>
 
