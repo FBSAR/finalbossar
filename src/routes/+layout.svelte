e360aa65fcdec5875e0acdc8fd4947b872bc009a
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2024-08-21 18:27:27 -0400

    Draft 1 of About Page / Navbar UI Refine (to purple)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,22 +3,22 @@
   import { page } from '$app/stores';
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Footer, FooterBrand, FooterCopyright, FooterIcon, FooterLink, FooterLinkGroup  } from 'flowbite-svelte';
   $: activeUrl = $page.url.pathname;
-  const nonActiveNavLink = 'text-[#777] duration-100 raleway';
-  const activeNavLink = 'text-[#DD0000] font-bold duration-100 raleway-700';
+  const nonActiveNavLink = 'text-[#fff]/60 duration-100 raleway';
+  const activeNavLink = 'text-[#00c400] font-bold duration-100 raleway-700';
 </script>
 <main>
-  <Navbar class="fixed w-full top-0 z-50 backdrop-blur-lg bg-white/90 shadow-md" color="none">
+  <Navbar class="fixed w-full top-0 z-50 backdrop-blur-lg bg-[#1b023d]/80 shadow-md" color="none">
       <NavBrand href="/">
         <img 
-          src="https://ik.imagekit.io/lgpq0vloy/logos/F_Logo.svg?updatedAt=1721187101575" 
+          src="https://ik.imagekit.io/lgpq0vloy/logos/F_Logo_White.png?updatedAt=1721187101575" 
           class="h-9 sm:h-12" 
           alt="Flowbite Logo" />
         <img 
-          src="https://ik.imagekit.io/lgpq0vloy/logos/Text_Logo.svg?updatedAt=1721187101565" 
+          src="https://ik.imagekit.io/lgpq0vloy/logos/Text_Logo_White.png?updatedAt=1721187101565" 
           class="h-6 sm:h-7" 
           alt="Flowbite Logo" />
       </NavBrand>
-      <NavHamburger  />
+      <NavHamburger menuClass={'text-white'}  />
       <NavUl {activeUrl}>
         <NavLi href="/" active={true} activeClass={activeNavLink} nonActiveClass={nonActiveNavLink}>HOME</NavLi>
         <NavLi href="/cosmic" active={true}  activeClass={activeNavLink} nonActiveClass={nonActiveNavLink}>COSMIC</NavLi>
@@ -34,11 +34,9 @@
 
   <Footer class="h-20 bg-black p-10">
     <div class="sm:flex sm:items-center sm:justify-between">
-    <FooterCopyright href="/" by="Flowbite™" year={2022} />
+    <FooterCopyright href="/" by="Final Boss Studios" year={2021} />
     <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
       <FooterLink href="/about">About</FooterLink>
-      <FooterLink href="/">Privacy Policy</FooterLink>
-      <FooterLink href="/">Licensing</FooterLink>
       <FooterLink href="/contact">Contact</FooterLink>
     </FooterLinkGroup>
     </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,11 +7,114 @@
     })
 </script>
 <main>
-  <!-- Page Wrapper -->
-  <div class="mx-auto h-20 bg-white w-11/12 lg:w-11/12 xl:w-10/12">
-    <h1 class="text-black">About Us</h1>
- 
+
+  <!-- Header -->
+  <div class="mx-auto w-11/12 lg:w-11/12 xl:w-10/12">
+    <h1 class="text-black text-4xl text-white">ABOUT US</h1>
+    <p class="sm:w-1/2 lg:w-1/2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium quis velit quibusdam molestias perspiciatis quidem ad, doloremque iure rem beatae magni harum tempore pariatur incidunt hic, alias ipsum dicta amet.</p>
+  </div>
+
+  <!-- Founders -->
+  <div class="mx-auto my-4 w-11/12 lg:w-11/12 xl:w-10/12">
+    <h1 class="text-white mb-2 mt-8">Founders</h1>
+    <div class="flex flex-wrap justify-start gap-2">
+      <div class="w-full rounded sm:w-1/2 lg:w-1/4 p-4 backdrop-blur-lg bg-white/10 border-white/20 border-2 text-white">
+        <img 
+          src="https://ik.imagekit.io/lgpq0vloy/Profile%20Pictures/eddie.jpg?updatedAt=1724278131402" 
+          class="h-40 rounded mb-2" 
+          alt="Flowbite Logo" 
+        />
+        <h1 class="text-2xl gold-header-text">Eddie Taliaferro II</h1>
+        <p class="border-b-2 border-gray-200/30 pb-1 mb-2">CEO / Co-Founder / Software Developer</p>
+        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima blanditiis officiis labore dolore rem veritatis eum pariatur alias doloremque? Iste, ad sed tempore non in modi maxime illo necessitatibus. Repudiandae.</p>
+      </div>
+      <div class="w-full rounded sm:w-1/2 lg:w-1/4 p-4 backdrop-blur-lg bg-white/10 border-white/20 border-2 text-white">
+        <img 
+          src="https://ik.imagekit.io/lgpq0vloy/Profile%20Pictures/fbs-keith.jpg?updatedAt=1724277860597" 
+          class="h-40 rounded mb-2" 
+          alt="Flowbite Logo" 
+        />
+        <h1 class="text-2xl gold-header-text">Keith Dunklin</h1>
+        <p class="border-b-2 border-gray-200/30 pb-1 mb-2">COO / Co-Founder / Game Developer</p>
+        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima blanditiis officiis labore dolore rem veritatis eum pariatur alias doloremque? Iste, ad sed tempore non in modi maxime illo necessitatibus. Repudiandae.</p>
+      </div>
+    </div>
    </div>
+
+  <!-- Team -->
+  <div class="mx-auto my-4 w-11/12 lg:w-11/12 xl:w-10/12">
+    <h1 class="text-white mb-2 mt-16">Team</h1>
+    <div class="flex flex-wrap justify-start gap-2">
+      <div class="w-full rounded sm:w-1/2 lg:w-1/4 p-4 backdrop-blur-lg bg-white/10 border-white/20 border-2 text-white">
+        <img 
+          src="https://ik.imagekit.io/lgpq0vloy/Profile%20Pictures/fbs-aaron.png?updatedAt=1724277860357" 
+          class="h-40 rounded mb-2" 
+          alt="Flowbite Logo" 
+        />
+        <h1 class="text-2xl gold-header-text">Aaron Goodson</h1>
+        <p class="border-b-2 border-gray-200/30 pb-1 mb-2">Senior Software Developer</p>
+        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima blanditiis officiis labore dolore rem veritatis eum pariatur alias doloremque? Iste, ad sed tempore non in modi maxime illo necessitatibus. Repudiandae.</p>
+      </div>
+      <div class="w-full rounded sm:w-1/2 lg:w-1/4 p-4 backdrop-blur-lg bg-white/10 border-white/20 border-2 text-white">
+        <img 
+          src="https://ik.imagekit.io/lgpq0vloy/Profile%20Pictures/fbs-richard.png?updatedAt=1724277860494" 
+          class="h-40 rounded mb-2" 
+          alt="Flowbite Logo" 
+        />
+        <h1 class="text-2xl gold-header-text">Richard Davis III</h1>
+        <p class="border-b-2 border-gray-200/30 pb-1 mb-2">Sound Design / 3D Modeling & Printing</p>
+        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima blanditiis officiis labore dolore rem veritatis eum pariatur alias doloremque? Iste, ad sed tempore non in modi maxime illo necessitatibus. Repudiandae.</p>
+      </div>
+      <div class="w-full rounded sm:w-1/2 lg:w-1/4 p-4 backdrop-blur-lg bg-white/10 border-white/20 border-2 text-white">
+        <img 
+          src="https://ik.imagekit.io/lgpq0vloy/Profile%20Pictures/rowan.png?updatedAt=1724278119089" 
+          class="h-40 rounded mb-2" 
+          alt="Flowbite Logo" 
+        />
+        <h1 class="text-2xl gold-header-text">Rowan Christerfield</h1>
+        <p class="border-b-2 border-gray-200/30 pb-1 mb-2">Social Media Handler / Creative Writer</p>
+        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima blanditiis officiis labore dolore rem veritatis eum pariatur alias doloremque? Iste, ad sed tempore non in modi maxime illo necessitatibus. Repudiandae.</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Advisors -->
+  <div class="mx-auto my-4 w-11/12 lg:w-11/12 xl:w-10/12">
+    <h1  class="text-white mb-2 mt-16">Advisors</h1>
+    <div class="flex flex-wrap justify-start gap-2">
+      <div class="w-full rounded sm:w-1/2 lg:w-1/4 p-4 backdrop-blur-lg bg-white/10 border-white/20 border-2 text-white">
+        <img 
+          src="https://ik.imagekit.io/lgpq0vloy/Profile%20Pictures/ed_kim.jpeg?updatedAt=1724277860376" 
+          class="h-40 rounded mb-2" 
+          alt="Flowbite Logo" 
+        />
+        <h1 class="text-2xl gold-header-text">Edward Kim</h1>
+        <p class="border-b-2 border-gray-200/30 pb-1 mb-2">CEO / Co-Founder / Software Developer</p>
+        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima blanditiis officiis labore dolore rem veritatis eum pariatur alias doloremque? Iste, ad sed tempore non in modi maxime illo necessitatibus. Repudiandae.</p>
+      </div>
+      <div class="w-full rounded sm:w-1/2 lg:w-1/4 p-4 backdrop-blur-lg bg-white/10 border-white/20 border-2 text-white">
+        <img 
+          src="https://ik.imagekit.io/lgpq0vloy/Profile%20Pictures/tim_bates.jpeg?updatedAt=1724277860159" 
+          class="h-40 rounded mb-2" 
+          alt="Flowbite Logo" 
+        />
+        <h1 class="text-2xl gold-header-text">Tim Bates</h1>
+        <p class="border-b-2 border-gray-200/30 pb-1 mb-2">CEO / Co-Founder / Software Developer</p>
+        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima blanditiis officiis labore dolore rem veritatis eum pariatur alias doloremque? Iste, ad sed tempore non in modi maxime illo necessitatibus. Repudiandae.</p>
+      </div>
+      <div class="w-full rounded sm:w-1/2 lg:w-1/4 p-4 backdrop-blur-lg bg-white/10 border-white/20 border-2 text-white">
+        <img 
+          src="https://ik.imagekit.io/lgpq0vloy/Profile%20Pictures/terrel_pic.png?updatedAt=1724277860626" 
+          class="h-40 rounded mb-2" 
+          alt="Flowbite Logo" 
+        />
+        <h1 class="text-2xl gold-header-text">Terrell Thomas</h1>
+        <p class="border-b-2 border-gray-200/30 pb-1 mb-2">CEO / Co-Founder / Software Developer</p>
+        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima blanditiis officiis labore dolore rem veritatis eum pariatur alias doloremque? Iste, ad sed tempore non in modi maxime illo necessitatibus. Repudiandae.</p>
+      </div>
+    </div>
+  </div>
+
 </main>
 <style>
 
