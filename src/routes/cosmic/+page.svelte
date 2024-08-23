0d97c189bc5f8fd4d4672ed8db1ce7951f47b891
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2024-08-22 20:00:32 -0400

    Added Green Gradient to all page headers

--- src/routes/cosmic/+page.svelte
+++ src/routes/cosmic/+page.svelte
@@ -9,7 +9,7 @@
 <main>
   <!-- Header -->
   <div class="mx-auto w-11/12 lg:w-11/12 xl:w-10/12">
-    <h1 class="text-black text-white jersey-font page-header">COSMIC COLLISIONS</h1>
+    <h1 class="jersey-font page-header green-header-text">COSMIC COLLISIONS</h1>
     <p class="sm:w-1/2 lg:w-1/2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium quis velit quibusdam molestias perspiciatis quidem ad, doloremque iure rem beatae magni harum tempore pariatur incidunt hic, alias ipsum dicta amet.</p>
   </div>
 </main>
