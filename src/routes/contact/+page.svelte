7dc2651ff175973a2bac240fc2e48f91998471fd
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2024-09-03 11:31:51 -0400

    Contact Form UI Refine

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { FloatingLabelInput, Textarea, Toast } from 'flowbite-svelte';
+  import { Badge, FloatingLabelInput, Textarea, Toast } from 'flowbite-svelte';
   import { CheckCircleOutline, CloseCircleOutline } from 'flowbite-svelte-icons'
   import { enhance } from '$app/forms';
 
   // TailwindCSS Classes
+  const inputClass = 'focus:bg-white/20 focus:border-2 focus:border-[#00FF00]';
   const successToastClass = 'w-full max-w-xl z-50 p-4 text-white text-3xl bg-black shadow dark:text-white dark:bg-black border-2 border-[#00ff00] rounded gap-3'
   const errorToastClass = 'w-full max-w-sm lg:max-w-xl z-50 p-4 text-white text-3xl bg-black shadow dark:text-white dark:bg-black border-2 border-[#dd0000] rounded gap-3'
 
@@ -72,22 +73,24 @@
 <!-- Header -->
 <div class="mx-auto px-4 container">
   <h1 class="jersey-font page-header green-header-text">CONTACT US</h1>
-  <p class="text-xs lg:text-xl sm:w-1/2 lg:w-1/2">
+  <p class="text-xs lg:text-lg sm:w-1/2 lg:w-1/2">
     Have a question? Have a comment? Want to work with, or invest in Final Boss?
-    Please reach out, and we will contact you are our earliest convenience.
+    Please reach out, and we will contact you are our earliest convenience!
   </p>
+  <Badge color="yellow">Please fill out entire form</Badge>
   <!-- Spacer -->
   <div class="h-4"></div>
    
   <!-- Form -->
-  <div class="w-full lg:w-1/2">
+  <div class="w-full lg:w-1/2 bg-white/10 p-4">
     <form method="POST" use:enhance on:submit|preventDefault={handleSubmit}>
       <FloatingLabelInput 
         maxlength="100"
         bind:value={contactInfo.name}
         name="name"
-        defaultClass={"mb-2"} style="filled" color="base" type="text">
-        Name
+        classInput={inputClass}
+        defaultClass={"mb-2 bg-red-900"} style="filled" type="text">
+        First & Last Name
       </FloatingLabelInput>
       <!-- Spacer -->
       <div class="h-4"></div>
@@ -95,7 +98,8 @@
         maxlength="100"
         bind:value={contactInfo.email}
         name="email"
-        defaultClass={"mb-2"} style="filled" color="base" type="text">
+        classInput={inputClass}
+        defaultClass={"mb-2"} style="filled" color="base" type="email">
         Email
       </FloatingLabelInput>
 
@@ -105,11 +109,15 @@
         maxlength="500"
         bind:value={contactInfo.message}
         name="message"
-        placeholder="Your message" rows="4"  />
+        class={inputClass}
+        placeholder="Your message" rows="6"  
+      />
+      <!--         style="background: var(--green-gradient);" 
+ -->
       <button
         type="submit"
-        style="background: var(--green-gradient);" 
-        class="block w-full h-10 rounded text-white text-lg mt-2 hover:bg-[var(--red)] duration-200">
+        disabled={contactInfo.name === '' || contactInfo.email === '' || contactInfo.message === '' }
+        class="block w-full h-10 rounded text-white text-lg mt-2 disabled:opacity-50 disabled:bg-white/20 bg-[#0e9f0e] hover:bg-[var(--red)] duration-200">
           Submit
       </button>
     </form>
@@ -118,7 +126,6 @@
   <!-- Toasts -->
   {#if successToast}
     <Toast 
-      position="top" 
       divClass={successToastClass} 
       contentClass={'w-full text-lg lg:text-2xl font-normal'} 
       dismissable={true} 
@@ -129,10 +136,8 @@
   {/if}
   {#if errorToast}
     <Toast 
-      position="top" 
       divClass={errorToastClass} 
       contentClass={'w-full text-lg lg:text-2xl font-normal'} 
-      transition={'fade'}
       dismissable={true} 
       align={true}>
         <span><CloseCircleOutline size="xl" color="#dd0000"></CloseCircleOutline></span>
